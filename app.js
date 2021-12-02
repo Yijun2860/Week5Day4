1c1ac69d4fe9b047c51bfc7beaeb07d6aabd1099
Yijun2860 <93628386+Yijun2860@users.noreply.github.com>
2021-12-01 22:02:54 -0500

    Week 5 Day 2 Project

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+$(':even').css('background-color', '#ccc');
+$(':odd').css('background-color', '#aaa');
+$('input').val('yijun');
